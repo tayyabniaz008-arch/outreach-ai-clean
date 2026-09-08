10c83c33cfcadb73bb02b8a60730b689f605c159
tayyabniaz008-arch <tayyabniaz008@gmail.com>
2026-09-08 13:38:03 -0700

    Fix: Prisma generate with Prisma 5

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+    // @ts-ignore
+    connectionLimit: 1,
+  });
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
