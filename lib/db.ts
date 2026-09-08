d6ff331dd4833ea7167aa2af61f3d353d8ce2c5a
tayyabniaz008-arch <tayyabniaz008@gmail.com>
2026-09-08 13:49:45 -0700

    Fix: Remove connectionLimit for Prisma 5

--- lib/db.ts
+++ lib/db.ts
@@ -2,12 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-    // @ts-ignore
-    connectionLimit: 1,
-  });
+export const db = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
