b8b3c036fc02368e0e8f46d36da4f7fa1901c3c7
tayyabniaz008-arch <tayyabniaz008@gmail.com>
2026-09-10 15:22:44 -0700

    Add followup fields to Prisma schema and migration

---
+++ prisma/migrations/20260910222135_add_followup_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Campaign" ADD COLUMN     "followupSubject1" TEXT,
+ADD COLUMN     "followupSubject2" TEXT,
+ADD COLUMN     "followupTemplate1" TEXT,
+ADD COLUMN     "followupTemplate2" TEXT;
